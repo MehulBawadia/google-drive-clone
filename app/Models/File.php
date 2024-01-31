2fcac672ebbd6938cf525611fc5387ef89322d1a
Mehul Bawadia <mehulbawadia@gmail.com>
2024-01-31 14:40:23 +0530

    Saving the folder path

--- app/Models/File.php
+++ app/Models/File.php
@@ -6,11 +6,42 @@ use App\Traits\HasCreatorAndUpdater;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 use Kalnoy\Nestedset\NodeTrait;
 
 class File extends Model
 {
     use HasCreatorAndUpdater, HasFactory, NodeTrait, SoftDeletes;
+
+    /**
+     * The file or folder belongs to a single user.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    /**
+     * The file or folder belongs to a single folder.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function parent()
+    {
+        return $this->belongsTo(File::class, 'parent_id');
+    }
+
+    /**
+     * Check if the folder is root folder.
+     *
+     * @return bool
+     */
+    public function isRoot()
+    {
+        return $this->parent_id === null;
+    }
 
     /**
      * Check if the file or folder is owned by the provided user id.
@@ -20,5 +51,23 @@ class File extends Model
     public function isOwnedBy($userId)
     {
         return $this->created_by === $userId;
+    }
+
+    /**
+     * Do something with the model events.
+     *
+     * @return void
+     */
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (! $model->parent) {
+                return;
+            }
+
+            $model->path = (! $model->parent->isRoot() ? $model->parent->path.'/' : '').Str::slug($model->name);
+        });
     }
 }
