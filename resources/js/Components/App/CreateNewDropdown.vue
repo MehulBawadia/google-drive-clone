e075c9a1cfb56a233baf8767416f0c843e356f37
Mehul Bawadia <mehulbawadia@gmail.com>
2024-02-09 13:03:04 +0530

    Disabled create new button except on my files page

--- resources/js/Components/App/CreateNewDropdown.vue
+++ resources/js/Components/App/CreateNewDropdown.vue
@@ -4,18 +4,26 @@ import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
 import CreateFolderModal from "@/Components/App/CreateFolderModal.vue";
 import FileUploadMenuItem from "@/Components/App/FileUploadMenuItem.vue";
 import FolderUploadMenuItem from "@/Components/App/FolderUploadMenuItem.vue";
+import { computed } from "vue";
+import { usePage } from "@inertiajs/vue3";
 
 const createFolderModal = ref(false);
 
 const showCreateFolderModal = () => {
     createFolderModal.value = true;
 };
+
+const btnDisabled = computed(() => {
+    return usePage().props.current_route !== "myFiles";
+});
 </script>
 
 <template>
     <Menu as="div" class="relative block text-left">
         <MenuButton
             class="flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+            :disabled="btnDisabled"
+            :class="btnDisabled ? 'disabled:opacity-50 cursor-not-allowed' : ''"
         >
             Create New
         </MenuButton>
