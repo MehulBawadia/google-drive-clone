ed13552c31ba3b2d5e0ce2ca3ead6f8f597b51e1
Mehul Bawadia <mehulbawadia@gmail.com>
2024-03-12 17:09:02 +0530

    SEO removed by adding noindex, nofollow

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="robots" content="NOINDEX, NOFOLLOW">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
@@ -15,7 +17,9 @@
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
+
     <body class="font-sans antialiased">
         @inertia
     </body>
+
 </html>
